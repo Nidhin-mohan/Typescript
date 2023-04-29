7a16b7410c93870703ce5f3f9e36b5e2a292e4ea
nidhin-mohan <nidhinmohannidhin@gmail.com>
2023-04-29 10:07:34 +0530

    lesson 2

---
+++ 00NetNinja/Finance logger/sandbox.js
@@ -0,0 +1,7 @@
+var char = 'Nikhil';
+console.log(char);
+var input = document.querySelectorAll('input');
+console.log(input);
+input.forEach(function (i) {
+    console.log(input);
+});
---
+++ 00NetNinja/Finance logger/sandbox.ts
@@ -0,0 +1,8 @@
+const char = 'Nikhil';
+console.log(char);
+const input = document.querySelectorAll('input');
+console.log(input);
+
+input.forEach(i => {
+    console.log(input);
+})
