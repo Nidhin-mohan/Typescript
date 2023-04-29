8723823047dd0f11961ae7089debd2463cd7a94f
nidhin-mohan <nidhinmohannidhin@gmail.com>
2023-04-29 10:18:36 +0530

    lesson 3 type basics

--- 00NetNinja/Finance logger/sandbox.js
+++ 00NetNinja/Finance logger/sandbox.js
@@ -1,7 +1,13 @@
-var char = 'Nikhil';
-console.log(char);
-var input = document.querySelectorAll('input');
-console.log(input);
-input.forEach(function (i) {
-    console.log(input);
-});
+var character = 'Nidhi';
+var age = 30;
+var isBlackBelt = false;
+// character = 20;
+character = 'something';
+// age = 'yos
+age = 34;
+// isBlackBelt = 'ye';
+isBlackBelt = false;
+var circ = function (diameter) {
+    return diameter * Math.PI;
+};
+console.log(circ(8));
--- 00NetNinja/Finance logger/sandbox.ts
+++ 00NetNinja/Finance logger/sandbox.ts
@@ -1,8 +1,16 @@
-const char = 'Nikhil';
-console.log(char);
-const input = document.querySelectorAll('input');
-console.log(input);
+let character = 'Nidhi';
+let age = 30;
+let isBlackBelt = false;
 
-input.forEach(i => {
-    console.log(input);
-})
+// character = 20;
+character = 'something';
+// age = 'yos
+age =34;
+// isBlackBelt = 'ye';
+isBlackBelt = false;
+
+const circ = (diameter : number) => {
+    return diameter * Math.PI;
+}
+
+console.log(circ(8))
