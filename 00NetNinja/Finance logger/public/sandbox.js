7f52bee33c26ad2682499319de9124ca7b52cd20
nidhin-mohan <nidhinmohannidhin@gmail.com>
2023-05-07 12:08:56 +0530

    lesson 8 functions

--- 00NetNinja/Finance logger/public/sandbox.js
+++ 00NetNinja/Finance logger/public/sandbox.js
@@ -1,2 +1,15 @@
 "use strict";
-console.log("firstfdhn667vd");
+let greet;
+greet = () => {
+    console.log("first");
+};
+// greet = "hello"
+const add = (a, b, c = 10) => {
+    console.log(a + b + c);
+};
+add(3, 7, 89);
+const minus = (a, b) => {
+    return a + b;
+};
+let result = minus(10, 7);
+// result = "some"
--- 00NetNinja/Finance logger/src/sandbox.ts
+++ 00NetNinja/Finance logger/src/sandbox.ts
@@ -1,1 +1,22 @@
-console.log("firstfdhn667vd")
+let greet : Function
+
+ greet = () => {
+    console.log("first")
+};
+
+// greet = "hello"
+
+const add = (a: number, b: number, c : number  = 10) : void => {
+  console.log(a+b+c)
+}
+
+add(3,7,89);
+
+
+const minus = (a:number, b:number) : number => {
+    return a+b;
+}
+
+let result = minus(10,7)
+// result = "some"
+
