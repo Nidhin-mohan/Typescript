257736267e71c0a751570d15e4ea579ec18f2a17
nidhin-mohan <nidhinmohannidhin@gmail.com>
2023-05-11 19:58:00 +0530

    lesson 10 Function signature

--- 00NetNinja/Finance logger/public/sandbox.js
+++ 00NetNinja/Finance logger/public/sandbox.js
@@ -1,7 +1,33 @@
 "use strict";
-const logDetails = (uid, item) => {
-    console.log(`${item} has uid of ${uid}`);
+// let  greet: Function
+// greet = 2;
+let greet;
+greet = (name, greeting) => {
+    console.log(`${name} say ${greeting}`);
 };
-const greet = (user) => {
-    console.log(`${name} says hello`);
+console.log(greet);
+let calc;
+calc = (numOne, numTwo, action) => {
+    if (action === '') {
+        return numOne + numTwo;
+    }
+    else {
+        return numOne - numTwo;
+    }
 };
+console.log(calc(3, 4, "void"));
+// let logDetails: (obj: {name:string, age: number}) => void
+// logDetails = (ninja : {
+//     name: "nidhin",
+//     age: 8
+// }) => {
+//     console.log(`${ninja.name} is ${ninja.age}`)
+// }
+let logDetails;
+logDetails = (ninja) => {
+    console.log(`${ninja.name} is ${ninja.age} years old.`);
+};
+logDetails({
+    name: "nidhin",
+    age: 9
+});
--- 00NetNinja/Finance logger/src/sandbox.ts
+++ 00NetNinja/Finance logger/src/sandbox.ts
@@ -1,13 +1,46 @@
-type StringOrNum = string | number;
-type objWithName = {
-    name: string,
-    uid: String
+// let  greet: Function
+
+// greet = 2;
+
+let greet : (a: string, b: string) => void;
+greet = (name: string, greeting: string) => {
+    console.log(`${name} say ${greeting}`)
 }
 
-const logDetails = (uid: string | number , item: string) => {
-    console.log(`${item} has uid of ${uid}`);
+
+console.log(greet)
+
+let calc: (a: number, b: number, c: string) => number
+
+calc = (numOne: number, numTwo: number, action: string) => {
+    if(action === ''){
+        return numOne+ numTwo
+    } else {
+        return numOne- numTwo
+    }
 }
 
-const greet = (user: objWithName) => {
-    console.log(`${name} says hello`)
+console.log(calc(3,4,"void"))
+
+
+// let logDetails: (obj: {name:string, age: number}) => void
+
+// logDetails = (ninja : {
+//     name: "nidhin",
+//     age: 8
+// }) => {
+//     console.log(`${ninja.name} is ${ninja.age}`)
+// }
+
+let logDetails: (obj: {name: string, age: number}) => void;
+
+type person = {name : string, age : number}
+
+logDetails = (ninja: person) => {
+    console.log(`${ninja.name} is ${ninja.age} years old.`);
 }
+
+logDetails({
+    name: "nidhin",
+    age: 9
+})
