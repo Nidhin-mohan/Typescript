e24e8cc5db793998f16e28ffdacb7e26f47da3c2
nidhin-mohan <nidhinmohannidhin@gmail.com>
2023-05-12 08:38:18 +0530

    lesson 11 The dom And type casting

---
+++ 00NetNinja/Finance logger/public/app.js
@@ -0,0 +1,16 @@
+"use strict";
+const anchor = document.querySelector('a');
+if (anchor) {
+    console.log(anchor.href);
+}
+console.log(anchor === null || anchor === void 0 ? void 0 : anchor.href);
+const form = document.querySelector(`form`);
+console.log(form.children);
+const type = document.querySelector("#type");
+const tofrom = document.querySelector("#tofrom");
+const details = document.querySelector("#details");
+const amount = document.querySelector("#amount");
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    console.log(type.value, tofrom.value, details.value, amount.valueAsNumber);
+});
