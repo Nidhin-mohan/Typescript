1d56cc9a05bf5bf42d1fdb0b82ee914b99aa222d
Nidhin-mohan <nidhinmohannidhin@gmail.com>
2023-06-03 19:59:05 +0530

    Tuples

---
+++ 02Basics/myTupEnums.ts
@@ -0,0 +1,15 @@
+// const user: (string | number)[] = [1, 'hc']
+let tUser: [string, number, boolean]
+
+tUser = ["hc", 4, true]
+
+let rgb: [number, number, number] = [233, 545, 645]
+
+type User = [number, string]
+
+ const newUser: User = [32, "nfi"]
+
+
+ newUser[1] = "hc.vom"
+
+ newUser.push(true)
