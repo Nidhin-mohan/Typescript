7b6a76845854df7e308ed55ee87af4985cbe9ee3
Nidhin-mohan <nidhinmohannidhin@gmail.com>
2023-06-10 09:46:33 +0530

    Enums and Interfaces

--- 02Basics/myInterfaces.ts
+++ 02Basics/myInterfaces.ts
@@ -1,36 +1,33 @@
 interface User {
-    readonly dbId : number,
-    email: string,
-    userId: number,
-    googleId?: string,
-    // startTrail: () => string
-    startTrail(): string,
-    getCoupon(couponName: string): number    
+  readonly dbId: number; // Represents a unique database identifier for the user (read-only)
+  email: string; // Represents the user's email address
+  userId: number; // Represents the user's ID
+  googleId?: string; // Represents the user's Google ID (optional)
+  startTrail(): string; // Represents a function to start a trail and returns a string
+  getCoupon(couponName: string): number; // Represents a function to get a coupon by name and returns a number
 }
 
 interface User {
-    gitHubId : string
+  gitHubId: string; // Represents the user's GitHub ID
 }
 
-interface admin extends User {
-    role: "admin" | "ta" | "learner"
-
+interface Admin extends User {
+  role: "admin" | "ta" | "learner"; // Represents the role of the admin, which can be "admin", "ta", or "learner"
 }
 
-const nidhinadmin : admin = {
-    dbId : 4,
-    gitHubId: "ba",
-    email: "nidn@ga",
-    userId: 4,
-    role: "admin",
-    startTrail : () => {
-        return "dffe"
-    },
-    getCoupon: (name: "fhoi") => {
-        return 12
-    }
-
-}
+const nidhinadmin: Admin = {
+  dbId: 4,
+  gitHubId: "ba",
+  email: "nidn@ga",
+  userId: 4,
+  role: "admin",
+  startTrail: () => {
+    return "dffe";
+  },
+  getCoupon: (name: "fhoi") => {
+    return 12;
+  },
+};
 
 const nidhin: User = {
   dbId: 4,
