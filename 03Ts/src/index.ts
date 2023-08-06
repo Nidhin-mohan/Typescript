66c661d57587e4cbcf43686209ab82f10fea786b
Nidhin-mohan <nidhinmohannidhin@gmail.com>
2023-08-06 08:00:29 +0530

    Classes in ts

--- 03Ts/src/index.ts
+++ 03Ts/src/index.ts
@@ -1,1 +1,12 @@
-console.log("hi ts welcome")
+class User {
+    email: string
+    name: string
+    readonly city: string = "readonly"
+    constructor(email:string, name: string){
+        this.email = email;
+        this.name = name;
+    }
+}
+
+const nidhin = new User("hi@f.com",  "uhfvu")
+// nidhin.city = "Kerala";
