0eec0d8ffda96a23a493ea8546f1f17bcc15f9c0
Nidhin-mohan <nidhinmohannidhin@gmail.com>
2023-08-06 10:18:34 +0530

    getters and setters

--- 03Ts/src/index.ts
+++ 03Ts/src/index.ts
@@ -1,13 +1,46 @@
+// class User {
+//     email: string
+//     name: string
+//     private readonly city: string = "readonly"
+//     constructor(email:string, name: string){
+//         this.email = email;
+//         this.name = name;
+//     }
+
+//     get getAppleEMail(): string{
+//         return `apple${this.email}`
+//     }
+// }
+
 class User {
-    email: string
-    name: string
-    private readonly city: string = "readonly"
-    constructor(email:string, name: string){
-        this.email = email;
-        this.name = name;
+    private _courseCount =  1
+  constructor(
+   public email: string, 
+   public  name: string
+    ) {
+   
+  }
+  private deleteToken(){
+    console.log("Token deleted")
+  }
+
+  get getAppleEMail(): string {
+    return `apple${this.email}`;
+  }
+
+  get courseCount(): number{
+    return this._courseCount;
+  }
+
+  set courseCount(courseNum){
+    if(courseNum <= 1){
+        throw new Error("course count should be more than one");
     }
+    this._courseCount = courseNum;
+  }
 }
 
-const nidhin = new User("hi@f.com",  "uhfvu")
+const nidhin = new User("hi@f.com",  "uhfvu");
 // nidhin.city 
-console.log(nidhin)
+console.log(nidhin);
+
