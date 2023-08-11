f034b528ab3d9260b38ca36dd622a076cfcbbc9b
Nidhin-mohan <nidhinmohannidhin@gmail.com>
2023-08-11 20:36:41 +0530

    Generics for classes

--- 03Ts/src/myGenerics.ts
+++ 03Ts/src/myGenerics.ts
@@ -25,4 +25,50 @@ identityTree("");
     type: number,
  }
 
- identityFour<Bootle>({})
+//  identityFour<Bootle>({})
+
+function getSerchProducts<T>(products: T[]):T{
+    // bbla blla
+    const myIndex = 3
+    return products[myIndex];
+}
+
+const getMoreSerchProducts = <T>(products: T[]): T=> {
+    //opifjviojae
+    const myData=  3;
+    return products[myData]
+}
+
+interface Database {
+    connection: string,
+    username: string,
+    password: string
+}
+
+function anotherFunction<T, U extends Database>(val1: T, val2: U):object{
+ return{
+    val1,
+    val2
+ }
+} 
+
+anotherFunction(3 , {});
+
+interface Quiz{
+    name: string,
+    type: string,
+}
+
+interface Course{
+    name: string,
+    author: string,
+    subject: string
+}
+
+class Sellable<T>{
+    public cart: T[] =[];
+
+    addToCart(products: T){
+        this.cart.push(products)
+    }
+}
